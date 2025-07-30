92e6218759a976f1bb8c3190c2df8251ed1427f5
Salaarkhaa9 <kowalskikh92@gmail.com>
2025-07-30 12:34:10 +0500

    Creating components

--- 3_tailwindprops/src/App.jsx
+++ 3_tailwindprops/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Cards from "./components/Cards"
 
 function App() {
-
+  const object={
+    username:"Salar",
+    age:18
+  }
+  let newArr=[12,34]
   return (
     <>
-    <h1 className='bg-green-500 text-black'>Tailwind tetsing</h1>
+      <h1 className="bg-green-500 text-black p-4 rounded-xl">
+        Tailwind tetsing
+      </h1>
+      {/* <Cards channel="chaiaurcode" someObj={object}/> */}
+      <Cards username="Taha" number="10"/>
+      <Cards username="Salar"/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
